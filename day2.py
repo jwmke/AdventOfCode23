b3cfc74e967a36fa8c3b4b622e4a7408b2daa21b
Joseph Weller <joseph5062a@gmail.com>
2023-12-02 09:28:47 -0500

    day2 p2

--- day2.py
+++ day2.py
@@ -9,20 +9,19 @@ class Solution:
             gn = i+1
             gs = p.split(";")
             a = True
+            mr, mg, mb = 0, 0, 0
             for g in gs:
                 s = g.split(",")
                 for ss in s:
                     fc = ss[len(ss)-1]
                     n = int(ss[1:].split(" ")[0])
-
-                    if fc == "d" and n > 12:
-                        a = False
-                    elif fc == "n" and n > 13:
-                        a = False
-                    elif fc == "e" and n > 14:
-                        a = False
-            if a:
-                t+= gn
+                    if fc == "d":
+                        mr = max(mr, n)
+                    elif fc == "n":
+                        mg = max(mg, n)
+                    elif fc == "e":
+                        mb = max(mb, n)
+            t += mr * mb * mg
         return t
 
 s = Solution()
