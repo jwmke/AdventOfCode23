1f6c1e395be9d771b6b7f464b43b9c80ea2b3195
Joseph Weller <joseph5062a@gmail.com>
2023-12-03 01:11:57 -0500

    day3 p2

--- day3.py
+++ day3.py
@@ -2,52 +2,56 @@ class Solution:
     def solve(self) -> int:
         file = open('input.txt', 'r')
         read = file.readlines()
-        symbols = {"/", "*", "$", "@", "%", "=", "&", "+", "-", "#"}
         matrix = []
         for l in read:
             matrix.append(l.replace("\n", ""))
 
-        def check(y, x) -> bool:
+        def check(y, x) -> (int, int):
             if y > 0:
-                if matrix[y-1][x] in symbols:
-                    return True
+                if matrix[y-1][x] == "*":
+                    return (y-1,x)
             if y < len(matrix)-1:
-                if matrix[y+1][x] in symbols:
-                    return True
+                if matrix[y+1][x] == "*":
+                    return (y+1,x)
             if x > 0:
-                if matrix[y][x-1] in symbols:
-                    return True
+                if matrix[y][x-1] == "*":
+                    return (y,x-1)
                 if y < len(matrix)-1:
-                    if matrix[y+1][x-1] in symbols:
-                        return True
+                    if matrix[y+1][x-1] == "*":
+                        return (y+1,x-1)
                 if y > 0:
-                    if matrix[y-1][x-1] in symbols:
-                        return True
+                    if matrix[y-1][x-1] == "*":
+                        return (y-1,x-1)
             if x < len(matrix[0])-1:
-                if matrix[y][x+1] in symbols:
-                    return True
+                if matrix[y][x+1] == "*":
+                    return (y,x+1)
                 if y < len(matrix)-1:
-                    if matrix[y+1][x+1] in symbols:
-                        return True
+                    if matrix[y+1][x+1] == "*":
+                        return (y+1,x+1)
                 if y > 0:
-                    if matrix[y-1][x+1] in symbols:
-                        return True
-            return False
+                    if matrix[y-1][x+1] == "*":
+                        return (y-1,x+1)
+            return None
 
+        seen = {}
         s = 0
         for y, l in enumerate(matrix):
-            pn = False
+            gc = None
             ns = []
             for x in range(len(l)):
                 if l[x].isnumeric():
                     ns.append(l[x])
-                    if (check(y, x)):
-                        pn = True
+                    r = check(y, x)
+                    if not r is None:
+                        gc = r
                 else:
-                    if pn:
-                        s += int("".join(ns))
+                    if gc:
+                        if gc in seen:
+                            s += seen[gc] * int("".join(ns))
+                        else:
+                            seen[gc] = int("".join(ns))
                     ns = []
-                    pn = False
+                    gc = None
         return s
 
 s = Solution()
