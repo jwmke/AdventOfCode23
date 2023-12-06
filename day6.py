bcf4296b2edec4db6b653cb24dc9197065eb6cfc
Joseph Weller <joseph5062a@gmail.com>
2023-12-06 00:22:43 -0500

    day6 p2

--- day6.py
+++ day6.py
@@ -1,15 +1,12 @@
 class Solution:
     def solve(self) -> int:
-        times = [42, 68, 69, 85]
-        distance = [284, 1005, 1122, 1341]
-        t = 1
-        for i in range(len(times)):
-            c = 0
-            for j in range(times[i]):
-                if ((j+1) * (times[i]-(j+1))) > distance[i]:
-                    c+=1
-            t*=c
-        return t
+        times = [42686985]
+        distance = [284100511221341]
+        c = 0
+        for j in range(times[0]):
+            if ((j+1) * (times[0]-(j+1))) > distance[0]:
+                c+=1
+        return c
 
 s = Solution()
 print(s.solve())
