87a1070aeb3bbca94864bbfa168ee506dcf78ff3
Joseph Weller <joseph5062a@gmail.com>
2023-12-13 14:14:10 -0500

    day13 p2

--- day13.py
+++ day13.py
@@ -13,14 +13,24 @@ class Solution:
                 ng.append(p)
         gs.append(ng)
 
+        def off_factor(f, s):
+            oc = 0
+            for fe, se in zip(f, s):
+                if fe != se:
+                    oc+=1
+            return oc
+
         def check_matrix(g):
             f = False
-            # horz check
             fn, fi = 0, 0
+            sf = False
             for si in range(1, len(g)):
                 # print(fi, si)
                 if not f:
-                    if g[fi] == g[si]:
+                    of = off_factor(g[fi], g[si])
+                    if of <= 1:
+                        if of == 1:
+                            sf = True
                         fn = si
                         f = True
                         nf = fi - 1
@@ -28,18 +38,36 @@ class Solution:
                         # print(fi, si, len(g))
                         for ns in range(si+1, len(g)):
                             if not nf < 0 and not ns > len(g) - 1:
-                                # print(g[nf] != g[ns])
-                                if g[nf] != g[ns]:
+                                sof = off_factor(g[nf], g[ns])
+                                # print(sf, sof, g[fi], g[nf])
+                                if sof > 1:
                                     f = False
                                     break
+                                elif sof == 1 and sf:
+                                    f = False
+                                    break
+                                elif not sf and sof == 1:
+                                    sf = True
+                                    nf -=1
                                 else:
                                     nf -= 1
+                                if ns == len(g)-1:
+                                    if sf or sof == 1:
+                                        return fn
                             else:
-                                return fn
+                                if sf:
+                                    return fn
+                                else:
+                                    f = False
+                                    break
+                        if sf and f:
+                            return fn
+                        sf = False
+                        f = False
                     else:
                         fi += 1
 
-            if f:
+            if f and sf:
                 return fn
             else:
                 return 0
@@ -50,10 +78,6 @@ class Solution:
             g = [list(i) for i in zip(*g)]
             r2 = check_matrix(g)
             t+=r2
-            if r == 0 and r2 == 0:
-                for l in g:
-                    print(l)
-                print("")
         
         return t
 
