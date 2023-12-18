96a8e4e15c819576decec21521ef8f924c48f202
Joseph Weller <joseph5062a@gmail.com>
2023-12-18 18:10:02 -0500

    day18 p2

--- day18.py
+++ day18.py
@@ -5,56 +5,37 @@ class Solution:
     def solve(self) -> int:
         file = open('input.txt', 'r')
         read = file.readlines()
-        ts = 800
         di = []
         for l in read:
             p = l.replace("\n", "").split(" ")
-            di.append((p[0], int(p[1]), p[2][2:-1]))
+            di.append((int(p[2][2:-1][:-1], 16), p[2][2:-1][-1]))
+            # di.append((int(p[1]), p[0]))
 
-        c = []
-        for y in range(ts):
-            cr = ['.' for _ in range(ts)]
-            c.append(cr)
-
-        cm = {}
-        ci = (ts//2, ts//2)
+        ed = []
+        ce = (0, 0)
+        tp = 0
         for d in di:
-            dr, dl = d[0], d[1]
-            if dr == 'R':
-                for i in range(dl):
-                    ci = (ci[0] + 1, ci[1])
-                    c[ci[1]][ci[0]] = "#"
-            elif dr == 'L':
-                for i in range(dl):
-                    ci = (ci[0] - 1, ci[1])
-                    c[ci[1]][ci[0]] = "#"
-            elif dr == 'U':
-                for i in range(dl):
-                    ci = (ci[0], ci[1] + 1)
-                    c[ci[1]][ci[0]] = "#"
-            elif dr == 'D':
-                for i in range(dl):
-                    ci = (ci[0], ci[1] - 1)
-                    c[ci[1]][ci[0]] = "#"
+            dl, dr = d[0], d[1]
+            tp += dl
+            ne = None
+            if dr == '0': #R
+                ne = (ce[0]+dl, ce[1])
+            elif dr == '2': #L
+                ne = (ce[0]-dl, ce[1])
+            elif dr == '3': #U
+                ne = (ce[0], ce[1]+dl)
+            elif dr == '1': #D
+                ne = (ce[0], ce[1]-dl)
+            ed.append((ce, ne))
+            ce = ne
 
-        se = set()
-        q = [((ts//2)+1, ts//2)]
-        while q:
-            x, y = q.pop()
-            c[y][x] = "#"
-            if c[y][x+1] == "." and (x+1, y) not in se:
-                q.append((x+1, y))
-            if c[y][x-1] == "." and (x-1, y) not in se:
-                q.append((x-1, y))
-            if c[y+1][x] == "." and (x, y+1) not in se:
-                q.append((x, y+1))
-            if c[y-1][x] == "." and (x, y-1) not in se:
-                q.append((x, y-1))
 
-        # for l in c:
-        #     print(''.join(l))
+        ts = 0
+        for e in ed:
+            (px, py), (nx, ny) = e
+            ts += (nx-px)*(ny+py)
 
-        return sum(x.count('#') for x in c)
+        return (ts//2) + (tp//2) + 1
 
 s = Solution()
 print(s.solve())
