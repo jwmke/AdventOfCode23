2a03176e7c9d7096322771934e0aa1ac66cf5840
Joseph Weller <joseph5062a@gmail.com>
2023-12-23 18:57:42 -0500

    day23 p2

--- day23.py
+++ day23.py
@@ -1,54 +1,73 @@
-import sys
+class Node:
+    def __init__(self, p):
+        self.p = p
+        self.es = {}
+        self.dr = None
+
+class Graph:
+    def __init__(self, g, s, e):
+        self.nd = {}
+        for p, v in g.items():
+            if v != '#':
+                self.nd[p] = Node(p)
+
+        self.s = self.nd[s]
+        self.e = self.nd[e]
+
+        for p, n in self.nd.items():
+            x, y = p
+            if (x+1, y) in self.nd:
+                n.es[self.nd[(x+1, y)]] = 1
+            if (x-1, y) in self.nd:
+                n.es[self.nd[(x-1, y)]] = 1
+            if (x, y+1) in self.nd:
+                n.es[self.nd[(x, y+1)]] = 1
+            if (x, y-1) in self.nd:
+                n.es[self.nd[(x, y-1)]] = 1
+
+        self.condense()
+
+    def solve(self) -> int:
+        r = 0
+        ss = [(self.s, 0, [])]
+        while ss:
+            n, dt, sn = ss.pop(-1)
+            if n == self.e:
+                r = max(r, dt)
+            for e in n.es:
+                if e not in sn:
+                    ss.append((e, dt + n.es[e], sn + [e]))
+        return r
+
+    def condense(self):
+        for n in self.nd.values():
+            if n.dr is not None: continue
+            if len(n.es) == 2 and not any(e.dr for e in n.es):
+                n1, n2 = n.es.keys()
+                del n1.es[n]
+                del n2.es[n]
+                n1.es[n2] = sum(n.es.values())
+                n2.es[n1] = sum(n.es.values())
 
 class Solution:
     def solve(self) -> int:
         file = open('input.txt', 'r')
         read = file.readlines()
-        sys.setrecursionlimit(100000)
-        g = []
+        g = {}
         s, e = None, None
         for i, l in enumerate(read):
-            if i == 0:
-                for j, c in enumerate(l):
+            p = l.replace("\n", "")
+            for j, c in enumerate(p):
+                g[(j, i)] = c
+                if i == 0:
                     if c == ".":
                         s = (j, 0)
-            elif i == len(l)-1:
-                for j, c in enumerate(l):
+                elif i == len(p)-1:
                     if c == ".":
-                        e = (j, len(l)-1)
-            p = l.replace("\n", "")
-            g.append(p)
+                        e = (j, len(p)-1)
 
-        def dfs(x, y, cv):
-            if (x, y) == e:
-                # for yy, l in enumerate(g):
-                #     st = []
-                #     for xx, c in enumerate(l):
-                #         if (xx, yy) in cv:
-                #             st.append("O")
-                #         else:
-                #             st.append(c)
-                #     print(''.join(st))
-                # print("")
-                return len(cv)
-            mt = 0
-            cs = g[y][x]
-            cv.add((x, y))
-            if (x+1, y) not in cv and g[y][x+1] != "#":
-                if cs == "." or cs == ">":
-                    mt = max(mt, dfs(x+1, y, cv.copy()))
-            if (x-1, y) not in cv and g[y][x-1] != "#":
-                if cs == "." or cs == "<":
-                    mt = max(mt, dfs(x-1, y, cv.copy()))
-            if (x, y+1) not in cv and g[y+1][x] != "#":
-                if cs == "." or cs == "v":
-                    mt = max(mt, dfs(x, y+1, cv.copy()))
-            if (x, y-1) not in cv and g[y-1][x] != "#":
-                if cs == "." or cs == "^":
-                    mt = max(mt, dfs(x, y-1, cv.copy()))
-            return mt
-
-        return dfs(s[0], s[1], {s})
+        ng = Graph(g, s, e)
+        return ng.solve()
 
 s = Solution()
 print(s.solve())
---
+++ day25.py
@@ -0,0 +1,11 @@
+class Solution:
+    def solve(self) -> int:
+        file = open('input.txt', 'r')
+        read = file.readlines()
+        for l in read:
+            p = l.replace("\n", "")
+
+        return 0
+
+s = Solution()
+print(s.solve())
