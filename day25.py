8c73c1a577e1a2b25d100607f10276d295f9db65
Joseph Weller <joseph5062a@gmail.com>
2023-12-25 03:10:54 -0500

    day25 p2

--- day25.py
+++ day25.py
@@ -1,11 +1,24 @@
+import networkx as nx
+
 class Solution:
     def solve(self) -> int:
         file = open('input.txt', 'r')
         read = file.readlines()
+        g = nx.DiGraph()
+        es = set()
         for l in read:
             p = l.replace("\n", "")
+            np = p.split(":")
+            for n in np[1][1:].split(" "):
+                g.add_edge(np[0], n, capacity=1.)
+                g.add_edge(n, np[0], capacity=1.)
+                es.add(n)
+            es.add(np[0])
 
-        return 0
+        for e in es:
+            cv, p = nx.minimum_cut(g, e[0], e[1])
+            if cv == 3:
+                return len(p[0])*len(p[1])
 
 s = Solution()
 print(s.solve())
