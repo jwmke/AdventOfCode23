c362ac6667b51a30522fc6070947a780c9af493f
Joe <joseph5062a@gmail.com>
2024-12-02 22:08:28 -0800

    day 3

--- 2024/day3.py
+++ 2024/day3.py
@@ -4,6 +4,7 @@ import copy
 
 def solve() -> int:
     sys.setrecursionlimit(200000)
+    
     t = 0
     s = "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
     do = True
