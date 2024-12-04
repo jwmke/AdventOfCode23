d81c37b99846100f541497c4e5ebe0c288111821
Joe <joseph5062a@gmail.com>
2024-12-03 21:47:40 -0800

    day 4

--- 2024/day4.py
+++ 2024/day4.py
@@ -7,11 +7,39 @@ def solve() -> int:
     file = open('input.txt', 'r')
     read = file.readlines()
     stripped = map(lambda s : s.replace("\n", ""), read)
+    m = []
     for i, l in enumerate(stripped):
-        # TODO - Save Christmas
-        pass
+        m.append(l)
 
-    return None
+    # def get_next(s):
+    #     if s == "X":
+    #         return "M"
+    #     if s == "M":
+    #         return "A"
+    #     if s == "A":
+    #         return "S"
+    #     if s == "S":
+    #         return "DONE"
+
+    def search(y, x):
+        if not (1 <= y < len(m)-1 and 1 <= x < len(m[0])-1):
+            return 0
+        ctl = m[y+1][x-1]
+        cbr = m[y-1][x+1]
+        if (ctl == "M" and cbr == "S") or (ctl == "S" and cbr == "M"):
+            ctr = m[y+1][x+1]
+            cbl = m[y-1][x-1]
+            if (ctr == "M" and cbl == "S") or (ctr == "S" and cbl == "M"):
+                return 1
+        return 0
+    
+    t = 0
+    for y in range(len(m)):
+        for x in range(len(m[0])):
+            if m[y][x] == "A":
+                t += search(y, x)
+    
+    return t
 
 if __name__ == '__main__':
     print(solve())
