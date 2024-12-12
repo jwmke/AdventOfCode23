ca67fdaa82bb01bffcc0ab2cd211d1a7dff193f6
Joe <joseph5062a@gmail.com>
2024-12-12 12:12:02 -0800

    corner start

--- 2024/day12.py
+++ 2024/day12.py
@@ -14,13 +14,15 @@ def solve() -> int:
     for i, l in enumerate(stripped):
         m.append(l)
 
-    global a, p
+    ds = [(1, 0),(0, -1), (0, 1),(-1, 0)]
+    
+    global a, c, blob
     def find_area(y, x):
-        global a
+        global a, blob
         plant = m[y][x]
         a+=1
         visited.add((y,x))
-        ds = [(1, 0),(0, -1), (0, 1),(-1, 0)]
+        blob.add((y,x))
 
         for dy, dx in ds:
             ny, nx = y + dy, x + dx
@@ -30,38 +32,29 @@ def solve() -> int:
                     if (ny, nx) not in visited:
                         find_area(ny, nx)
 
-    nm = {}
-    nm[(1, 0)] = (0, -1)
-    nm[(0, -1)] = (-1, 0)    
-    nm[(-1, 0)] = (0, 1)
-    nm[(0, 1)] = (1, 0)
-    
-
-    def find_cont_fences(y, x, dir, og, ogd, f):
-        global a, p
+    def find_corners(y, x):
+        global c
         plant = m[y][x]
-        if (y, x) == og and dir == ogd and not f:
-            return
-        dy, dx = dir
-        ny, nx = y + dy, x + dx
-        if 0 <= ny < len(m) and 0 <= nx < len(m[0]):
-            new_plant = m[ny][nx]
-            if new_plant == plant:
-                return find_cont_fences(ny, nx, dir, og, ogd, False)
-            else:
-                p+=1
-                return find_cont_fences(y, x, nm[dir], og, ogd, False)
-        else:
-            p+=1
-            return find_cont_fences(y, x, nm[dir], og, ogd, False)
+        visited.add((y,x))
+        corner_check_dict = {
+            (1, -1): [(1, 0), (0, -1)],
+            (1, 1): [(1, 0), (0, 1)],
+            (-1, -1): [(-1, 0), (0, -1)], 
+            (-1, 1): [(-1, 0), (0, 1)]
+        }
+
+        for dy, dx in ds:
+            ny, nx = y + dy, x + dx
+    
     
     for y in range(len(m)):
         for x in range(len(m[0])):
             if (y, x) not in visited:
-                a, p = 0, 0
+                a, p, blob = 0, 0, set()
                 find_area(y, x)
-                find_cont_fences(y, x, (1, 0), (y, x), (1, 0), True)
-                print(m[y][x], a, p)
+                visited = set()
+                find_corners(y, x)
+                print(m[y][x], a, c)
                 t += a * p
 
     return t
