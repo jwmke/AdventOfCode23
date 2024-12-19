3e527713d15b44a2c3c4b80ca66cbe76d8d0194e
Joe <joseph5062a@gmail.com>
2024-12-18 21:50:16 -0800

    day 19

--- 2024/day19.py
+++ 2024/day19.py
@@ -10,10 +10,32 @@ def solve() -> int:
     read = file.readlines()
     stripped = map(lambda s : s.replace("\n", ""), read)
     t = 0
+    ops = None
+    combos = []
     for i, l in enumerate(stripped):
-        # TODO - Save Christmas
-        pass
+        if i == 0:
+            ops = set(l.split(", "))
+            continue
+        if i == 1:
+            continue
+        combos.append(l)
 
+    @functools.cache
+    def find_valid(pat):
+        if pat == None or pat == "":
+            return 1
+        found = 0
+        for i in range(8):
+            if i < len(pat):
+                j = i + 1
+                ss = pat[:j]
+                if ss in ops:
+                    found = found + find_valid(pat[j:])
+        return found
+
+    for combo in combos:
+        t += find_valid(combo)
+    
     return t
 
 if __name__ == '__main__':
