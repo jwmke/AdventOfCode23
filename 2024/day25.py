1c99042dfb4b70c057540390c29c3c43bfda8ef1
Joseph Weller <joseph5062a@gmail.com>
2024-12-24 21:35:11 -0800

    day 25

--- 2024/day25.py
+++ 2024/day25.py
@@ -10,10 +10,50 @@ def solve() -> int:
     read = file.readlines()
     stripped = map(lambda s : s.replace("\n", ""), read)
     t = 0
-    for i, l in enumerate(stripped):
-        # TODO - Save Christmas
-        pass
+    
+    ll = set()
+    kl = set()
+    lms = []
+    kms = []
+    nc = 0
+    cm = []
 
+    def make_list():
+        if cm[0][0] == "#": # lock
+            cl = [0] * 5
+            for y in range(5):
+                for x in range(5):
+                    if cm[y+1][x] == "#":
+                        cl[x] += 1
+            lms.append(cl)
+        else: # key
+            cl = [0] * 5
+            for y in range(5):
+                for x in range(5):
+                    if cm[y+1][x] == "#":
+                        cl[x] += 1
+            kms.append(cl)
+
+    for i, l in enumerate(stripped):
+        if nc == 7:
+            make_list()
+            cm = []
+            nc = 0
+        else:
+            cm.append(l)
+            nc += 1
+
+    make_list()
+
+    for l in lms:
+        for k in kms:
+            f = True
+            for i in range(5):
+                if (l[i] + k[i]) > 5:
+                    f = False
+            if f:
+                t+=1
+        
     return t
 
 if __name__ == '__main__':
